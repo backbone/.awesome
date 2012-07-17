65e93c7ae5811647e7e4b55111bc02101febbbb9
Kolan Sh <mecareful@gmail.com>
2012-07-17 14:50:02 +0400

    Separate apprules.lua

---
+++ apprules.lua
@@ -0,0 +1,43 @@
+-- {{{ Rules
+awful.rules.rules = {
+    -- All clients will match this rule.
+    { rule = { },
+      properties = { border_width = beautiful.border_width,
+                     border_color = beautiful.border_normal,
+                     focus = true,
+                     keys = clientkeys,
+                     buttons = clientbuttons } },
+    { rule = { class = "URxvt" },
+      properties = { tag = tags[1][1] } },
+    { rule = { class = "Firefox" },
+      properties = { tag = tags[1][2] } },
+    { rule = { class = "VirtualBox" },
+      properties = { tag = tags[1][3] } },
+    { rule = { class = "Gimp" },
+      properties = { floating = true, tag = tags[1][4] } },
+    { rule = { class = "Soffice" },
+      properties = { floating = false, tag = tags[1][5] } },
+    { rule = { class = "libreoffice-writer" },
+      properties = { floating = false, tag = tags[1][5] } },
+    { rule = { class = "libreoffice-calc" },
+      properties = { floating = false, tag = tags[1][5] } },
+    { rule = { class = "libreoffice-draw" },
+      properties = { floating = false, tag = tags[1][5] } },
+    { rule = { class = "libreoffice-base" },
+      properties = { floating = false, tag = tags[1][5] } },
+    { rule = { class = "libreoffice-math" },
+      properties = { floating = false, tag = tags[1][5] } },
+    { rule = { class = "libreoffice-impress" },
+      properties = { floating = false, tag = tags[1][5] } },
+    { rule = { class = "libreoffice-startcenter" },
+      properties = { floating = false, tag = tags[1][5] } },
+    { rule = { class = "Pidgin" },
+      properties = { tag = tags[1][9] } },
+    { rule = { class = "Stardict" },
+      properties = { tag = tags[1][8] } },
+    { rule = { class = "MPlayer" },
+      properties = { floating = true } },
+}
+-- }}}
+
+
--- rc.lua
+++ rc.lua
@@ -371,47 +371,7 @@ clientbuttons = awful.util.table.join(
 root.keys(globalkeys)
 -- }}}
 
--- {{{ Rules
-awful.rules.rules = {
-    -- All clients will match this rule.
-    { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = true,
-                     keys = clientkeys,
-                     buttons = clientbuttons } },
-    { rule = { class = "URxvt" },
-      properties = { tag = tags[1][1] } },
-    { rule = { class = "Firefox" },
-      properties = { tag = tags[1][2] } },
-    { rule = { class = "VirtualBox" },
-      properties = { tag = tags[1][3] } },
-    { rule = { class = "Gimp" },
-      properties = { floating = true, tag = tags[1][4] } },
-    { rule = { class = "Soffice" },
-      properties = { floating = false, tag = tags[1][5] } },
-    { rule = { class = "libreoffice-writer" },
-      properties = { floating = false, tag = tags[1][5] } },
-    { rule = { class = "libreoffice-calc" },
-      properties = { floating = false, tag = tags[1][5] } },
-    { rule = { class = "libreoffice-draw" },
-      properties = { floating = false, tag = tags[1][5] } },
-    { rule = { class = "libreoffice-base" },
-      properties = { floating = false, tag = tags[1][5] } },
-    { rule = { class = "libreoffice-math" },
-      properties = { floating = false, tag = tags[1][5] } },
-    { rule = { class = "libreoffice-impress" },
-      properties = { floating = false, tag = tags[1][5] } },
-    { rule = { class = "libreoffice-startcenter" },
-      properties = { floating = false, tag = tags[1][5] } },
-    { rule = { class = "Pidgin" },
-      properties = { tag = tags[1][9] } },
-    { rule = { class = "Stardict" },
-      properties = { tag = tags[1][8] } },
-    { rule = { class = "MPlayer" },
-      properties = { floating = true } },
-}
--- }}}
+require("apprules")
 
 -- {{{ Signals
 -- Signal function to execute when a new client appears.
