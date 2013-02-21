85874ba9ee7b8a202f35d742050d2d61ba6e0efa
Kolan Sh <mecareful@gmail.com>
2013-02-21 14:29:40 +0400

    wallpaper.lua added

--- wallpaper.lua
+++ wallpaper.lua
@@ -1,1 +1,2 @@
-theme.wallpaper_cmd = { "awsetbg "..os.getenv("HOME").."/.config/awesome/wallpaper.png" }
+-- theme.wallpaper_cmd = { "awsetbg -a "..os.getenv("HOME").."/.config/awesome/wallpaper.png" }
+theme.wallpaper_cmd = { "awsetbg -r "..os.getenv("HOME").."/wallpapers" }
