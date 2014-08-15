47bb829b7b060a2faffa3f5e2fe2b25e13c07b77
Kolan Sh <backbone@backbone.ws>
2014-08-15 22:11:53 +0400

    Clean the config code.

--- mail_loop.sh
+++ mail_loop.sh
@@ -5,10 +5,10 @@ MAILDIR=/tmp/$USERNAME-mail_loop
 mkdir --mode=700 $MAILDIR
 
 while [ 1 ]; do
-	val=$(~/.config/awesome/mymail_unread.py)
+	val=$(~/.config/awesome/mail_loop_mymail.py)
 	echo $val > $MAILDIR/mymail_count
 	sleep 30
-	val=$(~/.config/awesome/gmail_unread.py)
+	val=$(~/.config/awesome/mail_loop_gmail.py)
 	echo $val > $MAILDIR/gmail_count
 	sleep 30
 done
