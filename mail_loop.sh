49e43b0cc2a03e9923e745afde031988f63a1fb3
Kolan Sh <backbone@backbone.ws>
2014-09-26 16:53:21 +0400

    Set timeout for the Mail Connections.

--- mail_loop.sh
+++ mail_loop.sh
@@ -2,13 +2,15 @@
 # mymail_gmail_loop.sh
 
 MAILDIR=/tmp/$USERNAME-mail_loop
-mkdir --mode=700 $MAILDIR
+mkdir --mode=700 $MAILDIR 2>/dev/null
 
 while [ 1 ]; do
-	val=$(~/.config/awesome/mail_loop_mymail.py)
+	val=$(timeout -k 30 25 ~/.config/awesome/mail_loop_mymail.py)
+	if [[ "$val" == "" ]]; then val = "?"; fi
 	echo $val > $MAILDIR/mymail_count
 	sleep 30
-	val=$(~/.config/awesome/mail_loop_gmail.py)
+	val=$(timeout -k 30 25 ~/.config/awesome/mail_loop_gmail.py)
+	if [[ "$val" == "" ]]; then val = "?"; fi
 	echo $val > $MAILDIR/gmail_count
 	sleep 30
 done
