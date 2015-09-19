abbd144dc326ffa95c90cbb42e7127682f9ceb81
Kolan Sh <backbone@backbone.ws>
2015-09-19 22:05:31 +0300

    Cinelerra tag.

--- rc.lua
+++ rc.lua
@@ -687,6 +687,8 @@ awful.rules.rules = {
       callback = function(c) awful.client.movetotag(tags[mouse.screen][6], c) end },
     { rule = { name = "d-feet" },
       callback = function(c) awful.client.movetotag(tags[mouse.screen][5], c) end },
+    { rule = { name = "Cinelerra" },
+      callback = function(c) awful.client.movetotag(tags[mouse.screen][6], c) end },
 }
 
 ----< Signals >------------------------------------------------------
